1e5d55c8c327ac645237ac4887f7b94a5ffb2da0
iReuters <982457331@qq.com>
2021-01-04 20:54:06 +0800

    Java data structure

--- src/com/company/Queue.java
+++ src/com/company/Queue.java
@@ -3,93 +3,147 @@ package com.company;
 public class Queue {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
-        System.out.println(linkedList.isEmpty());
-        linkedList.add(1, "宋江");
-        linkedList.add(2, "卢俊义");
-        linkedList.add(3, "吴用");
-        linkedList.add(4, "林冲");
+        linkedList.addByOrder(2, "宋江");
+        linkedList.addByOrder(4, "卢俊义");
+        linkedList.addByOrder(3, "吴用");
+        linkedList.addByOrder(1, "吴a");
         linkedList.show();
-        linkedList.delete("宋江");
-        linkedList.show();
+        System.out.println();
+
 
     }
 }
 
 class Node {
-    int n;
+    int no;
     String name;
     Node next;
 
-    public Node(int n, String name) {
-        this.n = n;
+    public Node(int no, String name) {
+        this.no = no;
         this.name = name;
     }
 }
 
 class LinkedList {
-    private Node dummy = new Node(0, "");
-    private Node head;
+    private Node head = new Node(0, "");
 
-    public boolean isEmpty() {
-        return dummy.next == null;
-    }
-
-    public void add(int n, String name) {
-        Node node = new Node(n, name);
-        if (isEmpty()) {
-            dummy.next = node;
+    public void add(int no, String name) {
+        Node temp = head.next;
+        if (temp == null){
+            head.next = new Node(no, name);
             return;
         }
-        traverse().next = node;
 
 
-    }
-
-    public void delete(String name) {
-        head = dummy;
-        if (isEmpty()) {
-            return;
-        }
-        while (head != null) {
-            if (head.next.name.equals(name)) {
+        while (true) {
+            if (temp.next != null) {
+                temp = temp.next;
+            }
+            else {
                 break;
             }
-            head = head.next;
         }
-        head.next = head.next.next;
+        temp.next = new Node(no, name);
     }
 
-    public Node research(String name) {
-        head = dummy.next;
-        if (isEmpty()) {
+    /**
+     * 链表反转
+     * @return 反转后链表头节点
+     */
+
+    public Node reverse() {
+        Node newHead = new Node(0, "");
+        Node temp = head.next;
+        Node temp1 = newHead.next;
+        if (temp == null) {
             return null;
         }
-        while (head != null) {
-            if (head.name.equals(name)) {
-                return head;
+        while (true) {
+            if (temp.next== null) {
+                return temp;
             }
-            head = head.next;
+            temp = temp.next;
         }
-        System.out.println("未找到该节点");
+
+        while (true) {
+            Node node = lastNode();
+            if (node != null) {
+
+            }
+        }
         return null;
     }
 
-    private Node traverse() {
-        head = dummy.next;
-        if (isEmpty()) {
+    public Node lastNode() {
+        Node temp = head.next;
+        if (temp == null) {
             return null;
         }
-        while (head.next != null) {
-            head = head.next;
+
+    }
+
+    public void addByOrder(int no, String name) {
+        Node temp = head.next;
+        if (temp == null){
+            head.next = new Node(no, name);
+            return;
         }
-        return head;
+        if (no <= 0 ) {
+            System.out.println("序号输入错误");
+            return;
+        }
+        while (true) {
+            if (temp.next == null){
+                temp.next = new Node(no, name);
+                return;
+            }else if (no == temp.next.no) {
+                System.out.printf("%s已经存在\n", name);
+                return;
+            }else if (no < temp.no) {
+                Node node = new Node(no, name);
+                node.next = temp;
+                head.next = node;
+                return;
+            }
+             else if (no < temp.next.no) {
+                Node node = new Node(no, name);
+                node.next = temp.next;
+                temp.next = node;
+                return;
+            }
+            temp = temp.next;
+        }
+    }
+
+    public Node lastIndexNode(int k) {
+        int len = lenth();
+        if (k <=0 || k > len) {
+            System.out.println("序号输入错误");
+            return null;
+        }
+        Node temp = head.next;
+        for (int i = 0; i < len - k; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public int lenth() {
+        int count = 0;
+        Node temp = head.next;
+        while (temp != null){
+            count++;
+            temp = temp.next;
+        }
+        return count;
     }
 
     public void show() {
-        head = dummy.next;
-        while (head != null) {
-            System.out.println(head.n + " " + head.name);
-            head = head.next;
+        Node temp = head.next;
+        while (temp != null) {
+            System.out.println(temp.no + " " + temp.name);
+            temp = temp.next;
         }
     }
 
