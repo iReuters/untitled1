fc5b790fe0a794bd13492c3a11db7d70f395ca33
iReuters <982457331@qq.com>
2021-01-07 20:09:12 +0800

    Java data structure

--- src/com/company/Queue.java
+++ src/com/company/Queue.java
@@ -3,14 +3,27 @@ package com.company;
 public class Queue {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
-        linkedList.addByOrder(2, "宋江");
-        linkedList.addByOrder(4, "卢俊义");
-        linkedList.addByOrder(3, "吴用");
-        linkedList.addByOrder(1, "吴a");
-        linkedList.show();
-        System.out.println();
+//        linkedList.addByOrder(2, "宋江");
+//        linkedList.addByOrder(4, "卢俊义");
+//        linkedList.addByOrder(3, "吴用");
+//        linkedList.addByOrder(1, "吴a");
+//        linkedList.show();
+//        System.out.println("反转后");
+//        linkedList.reverse();
+        Node l1 = new Node(9, "");
+        //l1.next = new Node(3, "");
+        Node l2 = new Node(9, "");
+        l2.next = new Node(9, "");
+        Node n = linkedList.addTwoNumbers(l1, l2);
+        show(n);
 
-
+    }
+    public static void show(Node head) {
+        Node temp = head.next;
+        while (temp != null) {
+            System.out.println(temp.no + " " + temp.name);
+            temp = temp.next;
+        }
     }
 }
 
@@ -27,6 +40,59 @@ class Node {
 
 class LinkedList {
     private Node head = new Node(0, "");
+
+    public Node addTwoNumbers(Node l1, Node l2) {
+        Node head = new Node(0, "");
+        Node t1 = l1;
+        Node t2 = l2;
+        int addOne = 0;
+        while (true) {
+            int add = ((t1.no + t2.no) % 10 + addOne) % 10;
+            int a = ((t1.no + t2.no) % 10 + addOne) / 10;
+            Node num = new Node(add, "");
+            addNumber(num, head);
+            addOne = (t1.no + t2.no) / 10 + a;
+            if (t1.next == null && t2.next == null) {
+                break;
+            }
+            if (t1.next != null) {
+                t1 = t1.next;
+            }else {
+                t1.no = 0;
+            }
+            if (t2.next != null) {
+                t2 = t2.next;
+            } else {
+                t2.no = 0;
+            }
+
+        }
+
+        if (addOne == 1) {
+            addNumber(new Node(addOne, ""), head);
+        }
+        return head;
+    }
+
+    private void addNumber(Node node, Node head) {
+        Node temp = head.next;
+        if (temp == null){
+            head.next = node;
+            return;
+        }
+
+
+        while (true) {
+            if (temp.next != null) {
+                temp = temp.next;
+            }
+            else {
+                break;
+            }
+        }
+        temp.next = node;
+    }
+
 
     public void add(int no, String name) {
         Node temp = head.next;
@@ -52,36 +118,31 @@ class LinkedList {
      * @return 反转后链表头节点
      */
 
-    public Node reverse() {
+    public void reverse() {
+        Node temp = head.next;
         Node newHead = new Node(0, "");
-        Node temp = head.next;
-        Node temp1 = newHead.next;
+        Node temp1 = null;
         if (temp == null) {
-            return null;
+            return;
         }
         while (true) {
-            if (temp.next== null) {
-                return temp;
+            Node node = new Node(temp.no, temp.name);
+            node.next = temp1;
+            newHead.next = node;
+            temp1 = newHead.next;
+            if (temp.next != null) {
+                temp = temp.next;
+                head.next = temp;
             }
-            temp = temp.next;
-        }
-
-        while (true) {
-            Node node = lastNode();
-            if (node != null) {
-
+            else {
+                showByHead(newHead);
+                return;
             }
+
         }
-        return null;
     }
 
-    public Node lastNode() {
-        Node temp = head.next;
-        if (temp == null) {
-            return null;
-        }
 
-    }
 
     public void addByOrder(int no, String name) {
         Node temp = head.next;
@@ -140,6 +201,18 @@ class LinkedList {
     }
 
     public void show() {
+        Node temp = head.next;
+        while (temp != null) {
+            System.out.println(temp.no + " " + temp.name);
+            temp = temp.next;
+        }
+    }
+
+    private void showByHead(Node head) {
+        if (head == null) {
+            System.out.println("空链表");
+            return;
+        }
         Node temp = head.next;
         while (temp != null) {
             System.out.println(temp.no + " " + temp.name);
